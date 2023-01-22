c9a64d4735ab6079c6dda8769e76f2c572caabc5
Tautvydas Gaizauskas <tautgaizauskas@gmail.com>
2023-01-22 16:48:17 +0200

    Added a list item view

---
+++ dev-Landmarks/dev-Landmarks/Views/LandmarkRow.swift
@@ -0,0 +1,31 @@
+//
+//  LandmarkRow.swift
+//  dev-Landmarks
+//
+//  Created by Tautvydas Gaižauskas on 2023-01-22.
+//
+
+import SwiftUI
+
+struct LandmarkRow: View {
+    var landmark: Landmark
+    
+    var body: some View {
+        
+        
+        HStack {
+            landmark.image
+                .resizable()
+                .frame(width: 50, height: 50)
+            Text(landmark.name)
+            
+            Spacer()
+        }
+    }
+}
+
+struct LandmarkRow_Previews: PreviewProvider {
+    static var previews: some View {
+        LandmarkRow(landmark: landmarks[0])
+    }
+}
