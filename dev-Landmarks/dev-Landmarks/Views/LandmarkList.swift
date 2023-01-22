5d11c25ae0eb3ac9c893cc69efcaad1edc5e8a6b
Tautvydas Gaizauskas <tautgaizauskas@gmail.com>
2023-01-22 18:40:25 +0200

    Section 8 done. Linked JSON file data to views

--- dev-Landmarks/dev-Landmarks/Views/LandmarkList.swift
+++ dev-Landmarks/dev-Landmarks/Views/LandmarkList.swift
@@ -12,7 +12,7 @@ struct LandmarkList: View {
         NavigationView {
             List(landmarks) { landmark in
                 NavigationLink {
-                    LandmarkDetail()
+                    LandmarkDetail(landmark: landmark)
                 } label: {
                     LandmarkRow(landmark: landmark)
                 }
@@ -24,6 +24,10 @@ struct LandmarkList: View {
 
 struct LandmarkList_Previews: PreviewProvider {
     static var previews: some View {
-        LandmarkList()
+        ForEach(["iPhone 14 Pro", "iPhone SE (3rd generation)", "iPad Air (5th generation)"], id: \.self) { deviceName in
+            LandmarkList()
+                .previewDevice(PreviewDevice(rawValue: deviceName))
+                .previewDisplayName(deviceName)
+        }
     }
 }
