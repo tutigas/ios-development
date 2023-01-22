6f5adb6d2fbc1776bc43a9108788feed80ce5817
Tautvydas Gaizauskas <tautgaizauskas@gmail.com>
2023-01-22 13:04:02 +0200

    Creating new files

---
+++ CreatingAndCombiningViews/Dev project: Landmarks/Dev project: Landmarks/Dev_project__LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  Dev_project__LandmarksApp.swift
+//  Dev project: Landmarks
+//
+//  Created by Tautvydas Gaižauskas on 2023-01-22.
+//
+
+import SwiftUI
+
+@main
+struct Dev_project__LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
