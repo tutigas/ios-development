178c9517439243ffe9fd89b4aaece61e06ddb2ec
Tautvydas Gaizauskas <tautgaizauskas@gmail.com>
2023-01-22 13:14:02 +0200

    Recreated project

---
+++ dev-Landmarks/dev-Landmarks/dev_LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  dev_LandmarksApp.swift
+//  dev-Landmarks
+//
+//  Created by Tautvydas Gaižauskas on 2023-01-22.
+//
+
+import SwiftUI
+
+@main
+struct dev_LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
