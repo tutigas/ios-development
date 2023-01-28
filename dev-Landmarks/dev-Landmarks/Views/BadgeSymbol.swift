499bfe288e1a06c485a946268640a009bff1e660
Tautvydas Gaizauskas <tautgaizauskas@gmail.com>
2023-01-28 18:38:16 +0200

    Started badge symbol file

---
+++ dev-Landmarks/dev-Landmarks/Views/BadgeSymbol.swift
@@ -0,0 +1,20 @@
+//
+//  BadgeSymbol.swift
+//  dev-Landmarks
+//
+//  Created by Tautvydas Gaižauskas on 2023-01-28.
+//
+
+import SwiftUI
+
+struct BadgeSymbol: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct BadgeSymbol_Previews: PreviewProvider {
+    static var previews: some View {
+        BadgeSymbol()
+    }
+}
